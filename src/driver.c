39b605a0aec4d78ea9673416522311496dfb427e
Bart <mrbart.tam@gmail.com>
2017-05-09 21:51:06 -0700

    Created parse_args file

--- src/driver.c
+++ src/driver.c
@@ -1,7 +1,13 @@
 
+// Used for loading dynamic libraries
 #include <dlfcn.h>
 
+#include <argp.h>
 
 int main(int argc, char *argv[]){
+
+
+  argp_parse(&parser, argc, argv, 0, 0, 0);
+
   return 0;
 }
---
+++ src/parse_args.c
@@ -0,0 +1,17 @@
+
+#ifndef DEC_PARSE_ARGS
+# define DEC_PARSE_ARGS
+
+#include <argp.h>
+
+const char *argp_program_version = "Decompiler TBC 0.1";
+const char *argp_program_bug_address = "<mrbart.tam@gmail.com>";
+
+typedef struct Arguments {
+
+} Arguments;
+
+void parse_args(){
+  
+}
+
